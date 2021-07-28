ffec900c48d808d7b5470c6b25b8679eefd53da4
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-07-28 13:44:24 +0530

    Update Matrix reading and printing.py

--- Matrix reading and printing.py
+++ Matrix reading and printing.py
@@ -13,3 +13,21 @@ for i in range(R):
 	for j in range(C):
 		print(matrix[i][j], end = " ")
 	print()
+"""
+o/p:
+Enter the number of rows:3
+Enter the number of columns:3
+Enter the entries rowwise:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+1 2 3 
+4 5 6 
+7 8 9
+"""
